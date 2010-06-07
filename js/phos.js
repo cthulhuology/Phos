c7a89c74803391c0fb53052d25d9e9a54e8e7333
David J. Goehrig <dave@nexttolast.com>
2010-06-07 18:21:18 -0400

    Adding AppleWebKit support for nightly build Moved all of Object Array String into objects remvoed adhoc.js

--- js/phos.js
+++ js/phos.js
@@ -24,6 +24,11 @@
 //	represent the various hardware devices that you might want to use, as well as, network
 //	resources.  It is designed around the concept of a simple object oriented statemachine.
 //
+
+function $(x) { return document.getElementById(x) }
+function $_(x) { return document.createElement(x) }
+document.goto = function(x) { document.location.href = x }
+
 var _doc = document;
 var _root = window;
 var _body = null;
@@ -31,6 +36,14 @@ var here;		// the last mouse position
 var that;		// the last thing selected
 var editing;		// the block we're editing
 var clipboard;		// the last object we cut or copied
+
+var A;
+var An = A = {
+	object: function() { return a({}) },
+	string: function() { return '' },
+	array: function() { return [] },
+};
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Event Functions
@@ -40,18 +53,55 @@ _root.onresize = function() { _doc.location.href = _doc.location.href };
 
 function nop() {}
 
+// Object Prototype Extensions necessary for bootstrapping
+Object.prototype.request = function(method,url,cb,data) {
+	var _request = XMLHttpRequest ? new XMLHttpRequest(): _doc.createRequest();
+	_request.onreadystatechange = function () {
+		if (this.readyState != 4 || typeof(cb) != "function") return;
+		if (this.status == 404) cb(null);
+		if (this.status == 200) cb(this.responseText);
+	};
+	_request.open(method,url,true);
+	_request.setRequestHeader('Content-Type','application/x-www-from-urlencoded');
+	_request.send(data ? data : '');
+	return this;
+}
+
+Object.prototype.get = function(url,cb) { return this.request("GET",url,cb) }
+
+Object.prototype.use = function() {
+	var modules = [];
+	modules.push.apply(modules,arguments);
+	var module = modules.shift();
+	var url = '/object/' + module;
+	var cb = function(txt) {
+		if (!txt) return;
+		try { 
+			eval('( function () { ' + txt + ' } )')(); 
+		} catch(e) { 
+			alert('Load error: ' + e + ':' + txt); ;
+		}
+		if (modules.length) {
+			var module = modules.shift();
+			var url = '/object/' + module;
+			if (module) this.get(url,cb);
+		}
+	};
+	return this.get(url,cb) 
+}
+	
 _root.onload = function() {
 	_body = _doc.getElementsByTagName('body')[0];
-	use('Box','Widget','Component','Event','Device','Resource',
-		'Display','Keyboard','Mouse','Screen',
-		'Text','Sound','Image','Movie',
+	use('Object','Array','String','Box','Widget','Component','Event','Device','Resource',
+		'Display','Keyboard','Mouse','Screen','Text','Sound','Image','Movie',
 		'Help','Names','HotKey','Block','Inventory',
 		'Objects','Phosphor','Graphic','Rectangle','Circle',
-		'Blog','Search','App');
-	navigator.userAgent.match(/Chrome/) ? use('Chrome') :
-	navigator.userAgent.match(/Safari/) ? use('Safari') :
-	navigator.userAgent.match(/Firefox/) ? use('Firefox') :
-		alert('Unsupported Browser');
+		'Blog','Search','App', 
+		navigator.userAgent.match(/AppleWebKit\/534/) ? 'WebKit' :
+		navigator.userAgent.match(/Safari/) ? 'Safari' :
+		navigator.userAgent.match(/Chrome/) ? 'Chrome' :
+		navigator.userAgent.match(/Firefox/) ? 'Firefox' : 
+		'Opera');
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
